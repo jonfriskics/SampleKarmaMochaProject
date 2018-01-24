495888c42bbc43005349031775244c6633c8445d
Hampton Paulk <hampton@pluralsight.com>
2018-01-24 09:31:59 -0500

    modify karma test to use assert instead of expect

--- test/unit/specs/part1/hello-world-renders-correct-contents.spec.js
+++ test/unit/specs/part1/hello-world-renders-correct-contents.spec.js
@@ -1,11 +1,12 @@
 import Vue from 'vue';
 import HelloWorld from '@/components/HelloWorld';
+const assert = require('chai').assert;
 
 describe('HelloWorld.vue', () => {
   it('should render correct contents @hello-world-renders-correct-contents', () => {
     const Constructor = Vue.extend(HelloWorld);
     const vm = new Constructor().$mount();
-    expect(vm.$el.querySelector('.hello h1').textContent)
-      .to.equal('Welcome to Your Vue.js App');
+    assert(vm.$el.querySelector('.hello h1'), 'The h1 was not found under the hello div in HelloWorld.vue');
+    assert(vm.$el.querySelector('.hello h1').textContent === 'Welcome to Your Vue.js App', 'Not rendering the correct copy inside of the h1 on HelloWorld.vue');
   });
 });
